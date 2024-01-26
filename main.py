4fa9be6261009f77dacbb74901781dbb5e7d5036
Daniel Briseño <20001426@alumnos.utleon.edu.mx>
2024-01-26 17:57:38 -0600

    JDSB-26/01/2024-Cinepolis  On branch Cinepolis  Changes to be committed: 	modified:   main.py 	new file:   static/css/cinepolis.css 	new file:   static/imgs/cinepolis.png 	new file:   static/js/cinepolis.js 	new file:   templates/cinepolis.html 	modified:   templates/layout.html

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template,request, jsonify
 
 app = Flask(__name__)
 
@@ -29,6 +29,49 @@ def mult():
             resultado = str(int(num1)/int(num2))
         
         return "<h1>El resultado de la {} es: {} </h1>".format(operacion, resultado)
+
+
+
+
+
+@app.route("/cinepolis")
+def cinepolis():
+    return render_template("cinepolis.html")
+
+@app.route('/procesar_formulario', methods=['POST'])
+def procesar_formulario():
+    nombre = request.form.get("nombre")
+    cantCompradores = request.form.get("cant-compradores")
+    tarjeta = request.form.get("tarjeta")
+    cantBoletos = request.form.get("cant-boletos")
+
+    if (nombre == "" or cantCompradores == "" or tarjeta == "" or cantBoletos == ""):
+        return jsonify({'estatus' : "error", 'mensaje': 'La información esta incompleta'})
+
+    if(int(cantCompradores) <= 0):
+        return jsonify({'estatus' : "error", 'mensaje': 'Los compradores no pueden ser {}'.format(cantCompradores)})
+    
+    if(int(cantBoletos) <= 0):
+        return jsonify({'estatus' : "error", 'mensaje': 'Los boletos no pueden ser {}'.format(cantBoletos)})
+
+    if( int(cantBoletos) > (int(cantCompradores)*7) ):
+        return jsonify({'estatus' : "error", 'mensaje': 'Solo se permite comprar {} boletos'.format(int(cantCompradores)*7)})
+
+    subTotal = int(cantBoletos)*12
+
+    if(int(cantBoletos) > 5):
+        total = subTotal - (subTotal*0.15)
+    elif(int(cantBoletos) >= 3 and int(cantBoletos) <= 5):
+        total = subTotal - (subTotal*0.10)
+    else:
+        total = subTotal
+
+    if(tarjeta == "si"):
+        total = total - (total*0.10)
+
+    return jsonify({'estatus': 'success' ,'comprador': nombre,'mensaje': '${0:.2f}'.format(total)})
+
+
 
 if __name__=="__main__":
     app.run(debug=True)
